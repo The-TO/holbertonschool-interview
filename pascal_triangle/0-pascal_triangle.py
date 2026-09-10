51fa3fcd330221de37ed13d27e3cfa0d44a69bb5
The-TO <theovinc@gmail.com>
2026-09-10 18:28:08 +0200

    fix: refactor pascal_triangle function for clarity and structure

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -5,21 +5,26 @@
 def pascal_triangle(n):
     """Algo pour le triangle de Pascale"""
     row = []
-    row.append([1])
-    row.append([1, 1])
-    prevlist = 1
-    while n-2 != 0:
-        index = 0 
-        new_row = []
-        new_row.append(1)
-        for _ in row[prevlist]:
-            try:
-                new_row.append(row[prevlist][index]+ row[prevlist][index+1])
-            except IndexError:
-                pass
-            index += 1
-        new_row.append(1)
-        row.append(new_row)
-        prevlist += 1
-        n -= 1
-    return row
+    if n <= 0:
+        return row
+    elif n == 1:
+        row.append([1])
+    else:
+        row.append([1])
+        row.append([1, 1])
+        prevlist = 1
+        while n-2 != 0:
+            index = 0 
+            new_row = []
+            new_row.append(1)
+            for _ in row[prevlist]:
+                try:
+                    new_row.append(row[prevlist][index]+ row[prevlist][index+1])
+                except IndexError:
+                    pass
+                index += 1
+            new_row.append(1)
+            row.append(new_row)
+            prevlist += 1
+            n -= 1
+        return row

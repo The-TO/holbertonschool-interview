d26acfb4351a3e36181f6a668469954f41d4cd68
The-TO <theovinc@gmail.com>
2026-09-15 16:43:05 +0200

    fix: improve formatting and readability of docstring in canUnlockAll function

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""fichier permettant de determiner si toutes les boites peuvent etres ouvertes"""
+"""fichier permettant de determiner si toutes les boites,
+peuvent etres ouvertes"""
+
 
 def canUnlockAll(boxes):
     n = len(boxes)
@@ -7,11 +9,11 @@ def canUnlockAll(boxes):
     unlocked = {0}
 
     keys = list(boxes[0])
-    
+
     while keys:
         current_key = keys.pop()
         if current_key < n and current_key not in unlocked:
-            unlocked.add (current_key)
-            keys.extend (boxes[current_key])
+            unlocked.add(current_key)
+            keys.extend(boxes[current_key])
 
     return len(unlocked) == n
